25981b1058ea11257141cea0cbf89feaf16b13c6
Sean Fisher <srtfisher@gmail.com>
2013-05-22 20:30:41 -0400

    API for versions

--- wphistory/Api.php
+++ wphistory/Api.php
@@ -12,3 +12,9 @@ $app->get('/api/current-version', function() use ($app) {
 		'url' => 'http://wordpress.org/latest.zip',
 	]));
 });
+
+$app->get('/api/all-versions', function() use ($app) {
+	$response = $app->response();
+	$response['Content-Type'] = 'application/json';
+	$response->body(json_encode(wphistory\Dotorg::getAllVersions()));
+});
--- wphistory/Dotorg.php
+++ wphistory/Dotorg.php
@@ -22,9 +22,42 @@ class Dotorg {
 	 * Get all versions
 	 *
 	 * @return array
+	 * @param string releases/beta-rc/mu
 	 */
-	public static function getAllVersions()
+	public static function getAllVersions($type = 'releases')
 	{
-		
+		$browser = new Browser();
+		$get = $browser->get('http://wordpress.org/download/release-archive/');
+
+		if (! $get->isOk())
+			die('error');
+
+		require_once WP_HISTORY_BASE.'/wphistory/HtmlDom.php';
+
+		$tbNum = 0;
+		if ($type == 'beta-rc')
+			$tbNum = 1;
+		elseif ($type == 'mu')
+			$tbNum = 2;
+
+		$dom = str_get_html($get->getContent());
+		$table = $dom->find('table.widefat', $tbNum);
+		$rows = $table->find('tr');
+		$count = -1;
+
+		$index = [];
+
+		foreach ($rows as $row) :
+			$count++;
+			if ($count == 0) continue;
+
+			$index[$count]['version'] = $row->find('td', 0)->innertext;
+			$index[$count]['zip'] = 'http://wordpress.org/wordpress-'.$index[$count]['version'].'.zip';
+			$index[$count]['tar'] = 'http://wordpress.org/wordpress-'.$index[$count]['version'].'.tar.gz';
+		endforeach;
+
+		return $index;
+
+		//var_dump($table->innertext);
 	}
 }
